9645afef592fb66fe38e08826ef476aeb8e240f5
Ankita Pasad <ankitap@slurm.ttic.edu>
2019-01-22 11:23:56 -0600

    multi-task learning model

--- model.py
+++ model.py
@@ -59,22 +59,22 @@ class VGG(nn.Module):
 
 
 class VISDNN(nn.Module):
-    def __init__(self, d_out, dropout):
+    def __init__(self, d_out, hidden_size, dropout): # d_out: 1000
         super(DNN, self).__init__()
         self.classifier = nn.Sequential(
-            nn.Linear(4096, 3072),
+            nn.Linear(4096, hidden_size),
             nn.ReLU(True),
             nn.Dropout(dropout),
-            nn.Linear(3072, 3072),
+            nn.Linear(hidden_size, hidden_size),
             nn.ReLU(True),
             nn.Dropout(dropout),
-            nn.Linear(3072, 3072),
+            nn.Linear(hidden_size, hidden_size),
             nn.ReLU(True),
             nn.Dropout(dropout),
-            nn.Linear(3072, 3072),
+            nn.Linear(hidden_size, hidden_size),
             nn.ReLU(True),
             nn.Dropout(dropout),
-            nn.Linear(3072, d_out))
+            nn.Linear(hidden_size, d_out))
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
@@ -119,8 +119,8 @@ class GLMDNN(nn.Module):
             w.data.uniform_(-math.sqrt(6.0/(hin+hout)), math.sqrt(6.0/(hin+hout)))
 
 
-class SpeechCNN(nn.Module):
-    def __init__(self, d_out, dropout):
+class SpeechCNNMT(nn.Module): # parallel
+    def __init__(self, d_out1, d_out2, dropout, modelType):
         # "filter_shapes": [
         #     [39, 9, 1, 64],
         #     [1, 10, 64, 256],
@@ -137,7 +137,8 @@ class SpeechCNN(nn.Module):
         input -> 16 (mini_batch) x 1 x 39 x 800 
         Conv2d -> inchannels, outchannels, kernel_size
         '''
-        super(SpeechCNN, self).__init__()
+        super(SpeechCNNMT, self).__init__()
+        self.type = modelType
         self.features = nn.Sequential(
             nn.Conv2d(1, 64, kernel_size=(39, 9)), # 16 x 64 x 1 x 792
             nn.ReLU(True),
@@ -150,25 +151,103 @@ class SpeechCNN(nn.Module):
             nn.MaxPool2d(kernel_size=(1, 75)) # 16 x 1024 x 1 x 1
         )
         self.classifierBoW = nn.Sequential(
-            nn.Linear(1024, 4096), # 16 x 4096 x 1 x 1
+            nn.Linear(1024, 4096), # 16 x 4096
             nn.Dropout(dropout),
             nn.ReLU(True),
-            nn.Linear(4096, vocab_size) # 16 x  (size of the vocbulary)
+            nn.Linear(4096, d_out2) # 16 x ~19000
         )
         self.classifierKW = nn.Sequential(
-            nn.Linear(1024, 4096), # 16 x 4096 x 1 x 1
+            nn.Linear(1024, 4096), # 16 x 4096
             nn.Dropout(dropout),
             nn.ReLU(True),
-            nn.Linear(4096, d_out) # 16 x 67 (# keywords)
+            nn.Linear(4096, d_out1) # 16 x 67 (# keywords)
         )
+        self.classifierBoW1 = nn.Sequential(
+            nn.Linear(1024, 4096), # 16 x 4096
+            nn.Dropout(dropout),
+            nn.ReLU(True)
+        )
+        self.classifierBoW2 = nn.Linear(4096, d_out2) # 16 x ~19000
+        self.classifierKW1 = nn.Sequential(
+            nn.Linear(4096, 512), # 16 x 4096
+            nn.Dropout(dropout),
+            nn.ReLU(True),
+            nn.Linear(512, d_out1) # 16 x 67 (# keywords)
+        )
+        self.classifierKW2 = nn.Linear(1024, d_out1) # 16 x 1000
+        self.features1 = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=(39, 9)), # 16 x 64 x 1 x 792
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=(1, 3)), # 16 x 64 x 1 x 264
+            nn.Conv2d(64, 256, kernel_size=(1, 10)), # 16 x 256 x 1 x 255
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=(1, 3)), # 16 x 256 x 1 x 85
+        )
+        self.classifierBoW3 = nn.Sequential(
+            nn.Conv2d(256, 4096, kernel_size=(1, 11)), # 16 x 4096 x 1 x 75
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=(1, 75)) # 16 x 1024 x 1 x 1
+        )
+        self.classifierKW3 = nn.Sequential(
+            nn.Conv2d(256, 1024, kernel_size=(1, 11)), # 16 x 4096 x 1 x 75
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=(1, 75)) # 16 x 1024 x 1 x 1
+        )       
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+
+        if(self.type=='paracnn'): x = self.features1(x)
+        else:
+            x = self.features(x)
+            x = x.view(-1, 1024)
+
+        if(self.type=='series'): 
+            y = self.classifierBoW1(x)
+            yBoW = self.classifierBoW2(y)
+            yKW = self.classifierKW1(y)
+
+        elif(self.type=='parallel'):
+            yBoW = self.classifierBoW(x)
+            yKW = self.classifierKW(x)
+
+        elif(self.type=='paracnn'):
+            xBoW, xKW = self.classifierBoW3(x), self.classifierKW3(x)
+            xBoW, xKW = xBoW.view(-1, 4096), xKW.view(-1, 1024)
+            yBoW, yKW = self.classifierBoW2(xBoW), self.classifierKW2(xKW)
+
+        probBoW, probKW = self.sigmoid(yBoW), self.sigmoid(yKW)
+
+        return probBoW, probKW
+
+class SpeechCNN(nn.Module):
+    def __init__(self, d_out, dropout):
+        # "filter_shapes": [
+        #     [39, 9, 1, 64],
+        #     [1, 10, 64, 256],
+        #     [1, 11, 256, 1024]
+        # ],
+        # "pool_shapes": [
+        #     [1, 3],
+        #     [1, 3],
+        #     [1, 75]
+        # ],
+        super(SpeechCNN, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=(39, 9)),
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=(1, 3)),
+            nn.Conv2d(64, 256, kernel_size=(1, 10)),
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=(1, 3)),
+            nn.Conv2d(256, 1024, kernel_size=(1, 11)),
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=(1, 75)))
         self.classifier = nn.Sequential(
-            nn.Linear(1024, vocab_size), # 16 x 4096 x 1 x 1
+            nn.Linear(1024, 4096),
             nn.Dropout(dropout),
             nn.ReLU(True),
-            nn.Linear(vocab_size, 4096), # 16 x 4096 x 1 x 1
-            nn.Dropout(dropout),
-            nn.ReLU(True),
-            nn.Linear(4096, d_out) # 16 x 67 (# keywords)
+            nn.Linear(4096, d_out)
         )
         self.sigmoid = nn.Sigmoid()
 
